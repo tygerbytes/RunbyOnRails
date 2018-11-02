e1905e608701fa62c70b2a9ca6fa07fd864f993f
Ty Walls <tygerbytes@tygertec.com>
2018-11-02 19:42:12 +0000

    Progress...

--- .docker_config/update_apt_sources.sh
+++ .docker_config/update_apt_sources.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 # Add good sources for postgres
-echo deb http://apt.postgresql.org/pub/repos/apt/ jessie-pgdg main > /etc/apt/sources.list.d/pgdg.list
+echo deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main > /etc/apt/sources.list.d/pgdg.list
 apt-key add ./.docker_config/ACCC4CF8.asc
 
-# Update the sources
-# apt-get update
